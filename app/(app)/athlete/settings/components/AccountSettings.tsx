b4cffc98a1852c8880e9987317bfc7da45360938
Aryan Kumar <aryankumar@Aryans-MacBook-Air.local>
2024-04-06 08:36:52 +0530

    FIX removal of important note on registeration type picker

--- app/(app)/athlete/settings/components/AccountSettings.tsx
+++ app/(app)/athlete/settings/components/AccountSettings.tsx
@@ -201,10 +201,6 @@ const AthleteAccountSettings = ({ athlete }: AthleteAccountSettingsProps) => {
               </option>
               <option value={AthleteRegistrationType.Team}>Team</option>
             </Select>
-            <span className="text-subtitle ">
-              Important Note: You can only be subscribed to either an individual
-              or a team plan at a time, but not both simultaneously.
-            </span>
           </div>
         </div>
 
